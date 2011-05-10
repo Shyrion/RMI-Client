e71264c82756fdec01d2893278c72f841a78b92f
Shyrion <jeremy.gabriele@gmail.com>
2011-05-10 11:21:48 +0200

    lilili

---
+++ src/client/Client.java
@@ -0,0 +1,30 @@
+package client;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
+import server.Distante;
+
+
+public class Client {
+
+	public static void main(String[] args) {
+		try {
+			//System.setSecurityManager(new java.rmi.RMISecurityManager());
+			Distante obj = (Distante) Naming.lookup("cr1");
+			obj.login("lol");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
