0d98b4fba3bbf73105fde2dcf68c146af38a1045
Claw <claw@claw-HP-ProBook-4720s.(none)>
2011-05-16 17:12:29 +0200

    le server ne crash plus

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,5 @@
 package client;
+
 import java.io.Console;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -9,52 +10,71 @@ import java.rmi.server.UnicastRemoteObject;
 
 import server.Distante;
 
-
 public class Client extends UnicastRemoteObject implements IClient {
 	private static final long serialVersionUID = 1L;
 	private String name;
 	private Distante chatroom;
-	
+	private ClientFrame frame;
+
 	protected Client() throws RemoteException {
 		super();
 		this.name = "";
 	}
-	
+
 	protected Client(String name) throws RemoteException {
 		super();
 		this.name = name;
 	}
 
 	public Distante getChatroom() throws RemoteException {
-     	return chatroom;
-     }
+		return chatroom;
+	}
 
-	public void setChatroom(Distante chatroom)  throws RemoteException {
-     	this.chatroom = chatroom;
-     }
+	public void setChatroom(Distante chatroom) throws RemoteException {
+		this.chatroom = chatroom;
+	}
+
+	public ClientFrame getFrame() {
+		return frame;
+	}
+
+	public void setFrame(ClientFrame frame) {
+		this.frame = frame;
+	}
 
 	public static void main(String[] args) {
 		try {
-			//System.setSecurityManager(new java.rmi.RMISecurityManager());
+			// System.setSecurityManager(new java.rmi.RMISecurityManager());
 			Distante cr1 = (Distante) Naming.lookup("cr1");
 			IClient client = new Client();
 			client.setChatroom(cr1);
 			Console console = System.console();
-			
-			if(console == null){
-				System.err.println("Error : No console ! Please run it with command line :).");
+
+			if (console == null) {
+				System.err
+				          .println("Error : No console ! Please run it with command line :).");
 			}
-			
+
 			String login = console.readLine("Please enter your login : ");
-			String password = console.readLine("Please enter your password : ");
-			
-			ClientFrame frame = new ClientFrame(client);
-			if(client.getChatroom().login(login, password, client)){
-				// switch sur la commande
-				//boucle qui le tient en vie, tu peux envoyer un message
+			String password = console
+			          .readLine("Please enter your password : ");
+
+			client.setFrame(new ClientFrame(client));
+			final IClient c2 = client;
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				public void run() {
+					try {
+						c2.getChatroom().logout(c2);
+					} catch (RemoteException e1) {
+						e1.printStackTrace();
+					}
+				}
+			});
+			if (client.getChatroom().login(login, password, client)) {
+				
 			}
 			return;
-			
+
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
@@ -63,27 +83,30 @@ public class Client extends UnicastRemoteObject implements IClient {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public void notify(String message) throws RemoteException{
+
+	public void notify(String message) throws RemoteException {
 		System.out.println(message);
+		frame.addMessage(message);
 	}
-	
-	public void notifyConnect(String message) throws RemoteException{
+
+	public void notifyConnect(String message) throws RemoteException {
 		notify(message + " has joined the channel.");
+		frame.addMessage(message);
 		// set la liste des gens connectés
 	}
-	
-	public void notifyDisconnect(String message) throws RemoteException{
+
+	public void notifyDisconnect(String message) throws RemoteException {
 		notify(message + " has left the channel.");
+		frame.addMessage(message);
 		// set la liste des gens connectés
 	}
-	
+
 }
--- src/client/ClientFrame.java
+++ src/client/ClientFrame.java
@@ -1,10 +1,14 @@
 package client;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.rmi.RemoteException;
 
 import javax.swing.DefaultListModel;
@@ -16,70 +20,107 @@ import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 
-public class ClientFrame extends JFrame {
+public class ClientFrame extends JFrame implements IClientFrame {
 	private JPanel mainPanel;
-     private JSplitPane horizontalSplitPane;
-     private JSplitPane verticalSplitPane;
-     private JScrollPane messagePane;
-     private JPanel inputPanel;
-     
-     private JScrollPane usersPane;
-     private JList usersJList;
-     private DefaultListModel usersModel;
+	private JSplitPane horizontalSplitPane;
+	private JSplitPane verticalSplitPane;
 
-     private JTextArea inputArea;
-     private JButton sendButton;
-     
-     private IClient client;
-     
-     public ClientFrame(IClient client){
-     	this.client = client;
-     	buildFrame();
-     }
-	
-     private void buildFrame() {
-     	mainPanel = new JPanel(new BorderLayout());
-     	
-     	usersModel = new DefaultListModel();
-     	usersJList = new JList(usersModel);
-     	usersPane = new JScrollPane(usersJList);
-     
-     	messagePane = new JScrollPane();
-     	
-     	horizontalSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, messagePane, usersPane);
-     	horizontalSplitPane.setDividerLocation(600);
-     	horizontalSplitPane.setEnabled(false);
-     	inputPanel = new JPanel();
-     	inputPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-     	inputArea = new JTextArea(6, 60);
-     	inputPanel.add(inputArea);
-     	sendButton = new JButton("Send");
-     	inputPanel.add(sendButton);
-     	
-     	sendButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
+	private JPanel messagePanel;
+	private JScrollPane messageScrollPane;
+	private JTextArea messageArea;
+
+	private JScrollPane usersPane;
+	private JList usersJList;
+	private DefaultListModel usersModel;
+
+	private JPanel inputPanel;
+	private JTextArea inputArea;
+	private JButton sendButton;
+
+	private IClient client;
+
+	public ClientFrame(IClient client) {
+		this.client = client;
+		buildFrame();
+	}
+
+	public void buildFrame() {
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
 				try {
-	                    client.getChatroom().broadCastMessage(client, inputArea.getText());
+	                    client.getChatroom().logout(client);
                     } catch (RemoteException e1) {
 	                    e1.printStackTrace();
                     }
 			}
 		});
-     	
-     	verticalSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, horizontalSplitPane, inputPanel);
-     	verticalSplitPane.setDividerLocation(450);
-     	verticalSplitPane.setEnabled(false);
-     	
-     	mainPanel.add(verticalSplitPane);
-     	
-          this.setMinimumSize(new Dimension(800, 600));
-          this.setSize(600, 500);
-          this.setResizable(false);
-          this.setLocationRelativeTo(null);
-          this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          this.setTitle("ChatRoom");
-          this.setContentPane(mainPanel);
-          this.setVisible(true);
-     }
+		
+		mainPanel = new JPanel(new BorderLayout());
+
+		usersModel = new DefaultListModel();
+		usersJList = new JList(usersModel);
+		usersPane = new JScrollPane(usersJList);
+
+		messagePanel = new JPanel();
+		messagePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		messageArea = new JTextArea(29, 47);
+		messageArea.setEditable(false);
+		Font font = new Font("Verdana", Font.BOLD, 12);
+		messageArea.setFont(font);
+		messageArea.setForeground(Color.BLACK);
+		messageScrollPane = new JScrollPane(messageArea,
+		          JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+		          JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		messagePanel.add(messageScrollPane);
+
+		horizontalSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+		          messagePanel, usersPane);
+		horizontalSplitPane.setDividerLocation(600);
+		horizontalSplitPane.setEnabled(false);
+		inputPanel = new JPanel();
+		inputPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		inputArea = new JTextArea(6, 60);
+		inputPanel.add(inputArea);
+		sendButton = new JButton("Send");
+		inputPanel.add(sendButton);
+
+		sendButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					client.getChatroom().broadCastMessage(client,
+					          inputArea.getText());
+					inputArea.setText("");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		verticalSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+		          horizontalSplitPane, inputPanel);
+		verticalSplitPane.setDividerLocation(450);
+		verticalSplitPane.setEnabled(false);
+
+		mainPanel.add(verticalSplitPane);
+
+		this.setMinimumSize(new Dimension(800, 600));
+		this.setSize(800, 600);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("ChatRoom");
+		this.setContentPane(mainPanel);
+		this.setVisible(true);
+	}
+
+	public void addMessage(String message) {
+		messageArea.setText(messageArea.getText() + "\n" + message);
+		int x;
+		messageArea.selectAll();
+		messageArea.setSelectedTextColor(Color.black);
+		x = messageArea.getSelectionEnd();
+		messageArea.select(x,x);
+		messageArea.repaint();
+	}
 }
